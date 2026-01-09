6faf6d728b48f54ae8981d9f64e7b7c206257b85
Augusto Ribeiro <augustoribeiro100.g.j@gmail.com>
2026-01-08 21:43:32 -0300

    first commit

---
+++ src/main/java/com/emprestimolivro/api_emprestimolivro/ApiEmprestimolivroApplication.java
@@ -0,0 +1,13 @@
+package com.emprestimolivro.api_emprestimolivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmprestimolivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmprestimolivroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emprestimolivro/api_emprestimolivro/ApiEmprestimolivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emprestimolivro.api_emprestimolivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmprestimolivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
